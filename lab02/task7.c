f0f826af6a40aff6179ab47d4bed59a04d3150e6
Ivan <irubanov45@gmal.com>
2026-04-01 12:50:36 +0300

    fix task7

--- lab02/task7.c
+++ lab02/task7.c
@@ -2,9 +2,8 @@
 
 enum course {FIRST, SECOND, THIRD, FOURTH};
 
-struct Student
-{
-    char * name;
+struct Student {
+    char name[100]; 
     int age;
     enum course current_course;
     float score;
@@ -59,8 +58,14 @@ void returnListOfStudents(struct Student* students, int size){
     }
 }
 
-struct Student* addNewStudent(struct Student* students, int count, char* newname, int newage, enum course newcourse, float newscore){
-    students[count].name = newname;
+void addNewStudent(struct Student* students, int count, char* newname, int newage, enum course newcourse, float newscore) {
+    int i = 0;
+    while (newname[i] != '\0') {
+        students[count].name[i] = newname[i];
+        i++;
+    }
+    students[count].name[i] = '\0'; 
+    
     students[count].age = newage;
     students[count].current_course = newcourse;
     students[count].score = newscore;
@@ -68,11 +73,10 @@ struct Student* addNewStudent(struct Student* students, int count, char* newname
 
 int main(){
     int count = 1;
-    returnListOfStudents(students, count);
 
     char current_name[100];
     printf("введите имя студента\n");
-    scanf("%99s", &current_name);
+    scanf("%99s", current_name);
     int age;
     float score;
     int intcourse;
@@ -81,20 +85,47 @@ int main(){
 
     printf("введите курс студента(1-4)\n");
     scanf("%d", &intcourse);
-    enum course curcourse = intcourse-1;
 
     printf("введите балл студента\n");
     scanf("%f", &score);
 
-    addNewStudent(students, count++, current_name, age, curcourse, score);
+    addNewStudent(students, count++, current_name, age, (enum course)(intcourse - 1), score);
 
-    printf("\n");
-    printf("список студнетов до сортировки\n");
-    returnListOfStudents(students, count);
+
+    printf("введите имя 2 студента\n");
+    scanf("%99s", &current_name);
+
+    printf("введите возраст 2 студента\n");
+    scanf("%d", &age);
+
+    printf("введите курс 2 студента(1-4)\n");
+    scanf("%d", &intcourse);
+
+    printf("введите балл 2 студента\n");
+    scanf("%f", &score);
+
+    addNewStudent(students, count++, current_name, age, (enum course)(intcourse - 1), score);
+
+    
+    printf("_________________________________\n");
+    printf("введите имя студента для поиска\n");
+    char nameForFind[100];
+    scanf("%99s", nameForFind);
+    struct Student* found = findForName(students, count, nameForFind);
+    if (found != 0) {
+        printf("\nСтудент найден\n");
+        printf("Имя: %s\n", found->name);
+        printf("Возраст: %d\n", found->age);
+        printf("Курс: %d\n", found->current_course + 1);
+        printf("Средний балл: %.2f\n", found->score);
+    } else {
+        printf("\nСтудент с именем '%s' не найден в списке\n", nameForFind);
+    }
+
 
     sort(students, count);
     
-    printf("\n");
+    printf("_________________________________\n");
     printf("список студнетов после сортировки\n");
     returnListOfStudents(students, count);
     return 0;
